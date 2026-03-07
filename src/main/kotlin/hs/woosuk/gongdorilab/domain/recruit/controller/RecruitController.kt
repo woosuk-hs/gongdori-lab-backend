9e656d39c3c13387517daea6c64adfa1bd869def
mrjimin <aa090402@naver.com>
2026-03-07 23:30:26 +0900

    commit

--- src/main/kotlin/hs/woosuk/gongdorilab/domain/recruit/controller/RecruitController.kt
+++ src/main/kotlin/hs/woosuk/gongdorilab/domain/recruit/controller/RecruitController.kt
@@ -28,7 +28,7 @@ class RecruitController(
     fun index(): List<RecruitResponseDTO> =
         recruitService.findAll().map { it.toResponseDTO() }
 
-    @PatchMapping("/{studentId}/status")
+    @PatchMapping("/status{studentId}")
     fun updateStatusByStudentId(
         @PathVariable studentId: String,
         @RequestBody dto: RecruitUpdateDTO
